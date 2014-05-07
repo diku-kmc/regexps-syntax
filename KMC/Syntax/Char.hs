6e126b05758d96d20251f904526c0beaf0f570ee
Niels Bjørn Bugge Grathwohl <bugge@diku.dk>
2014-05-07 15:13:49 +0200

    Any-case string matching.

---
+++ KMC/Syntax/Char.hs
@@ -0,0 +1,15 @@
+module KMC.Syntax.Char where
+
+import           Data.Char                          (toLower, toUpper)
+import           Text.Parsec.Prim                   (try, (<?>), (<|>))
+import           Text.ParserCombinators.Parsec.Char (char)
+
+import           KMC.Syntax.ParserTypes
+
+-- | Match the lowercase or uppercase form of 'c'
+caseInsensitiveChar :: Char -> Parser Char
+caseInsensitiveChar c = char (toLower c) <|> char (toUpper c)
+
+-- | Match an any-cased version of a string.
+caseInsensitiveString :: String -> Parser String
+caseInsensitiveString s = try (mapM caseInsensitiveChar s) <?> "\"" ++ s ++ "\""
